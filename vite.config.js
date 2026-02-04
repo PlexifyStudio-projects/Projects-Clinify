d7adcca4f35a5eba94078323c0a3193ce99d9d40
LJ-Nava <developerluis17@gmail.com>
2026-02-04 11:00:21 -0500

    Update base URL for new GitHub account

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'build' ? '/Health/' : '/',
+  base: command === 'build' ? '/Projects-Clinify/' : '/',
   server: {
     port: 3000,
     open: true
